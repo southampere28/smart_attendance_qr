d4d700362b78696c173e4fc8bb3e802b9ccdc49b
Pramudya <pramudyap333@gmail.com>
2025-09-08 21:51:54 +0700

    feat: update request return type to ApiResult object

--- lib/services/endpoint_service.dart
+++ lib/services/endpoint_service.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:absensi_qr/configs/api_constant.dart';
 import 'package:absensi_qr/constant/app_config.dart';
+import 'package:absensi_qr/models/response/api_result.dart';
 import 'package:absensi_qr/models/user/user.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
@@ -33,13 +34,13 @@ class EndpointService extends GetxService {
     }
   }
 
-  Future<bool> login({
+  Future<ApiResult<Map<String, dynamic>>> login({
     required String email,
     required String password,
   }) async {
     try {
       final response = await http.post(
-        Uri.parse(ApiConstant.loginURL), // pastiin ada di ApiConstant
+        Uri.parse(ApiConstant.loginURL),
         headers: {
           "Accept": "application/json",
         },
@@ -49,29 +50,40 @@ class EndpointService extends GetxService {
         },
       );
 
+      final status = response.statusCode;
+      final data = jsonDecode(response.body);
+
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
 
         // Simpan token & user
         accessToken = data["access_token"];
         tokenType = data["token_type"];
         userData = data["user"];
 
-        Fluttertoast.showToast(msg: 'Login success!');
-
         log("Token: $accessToken");
         log("User: ${userData.toString()}");
 
-        return true;
+        return ApiResult(
+          success: data["success"] ?? true,
+          data: userData,
+          message: data["message"],
+          statusCode: status,
+        );
       } else {
-        Fluttertoast.showToast(msg: 'Login failed!');
         log("Login error: ${response.body}");
-        return false;
+        return ApiResult(
+          success: false,
+          message: data["message"],
+          statusCode: status,
+        );
       }
     } catch (e) {
-      Fluttertoast.showToast(msg: 'Login error!');
       log("Exception: $e");
-      return false;
+      return ApiResult(
+          success: false,
+          message: "Exception: $e",
+          statusCode: null,
+        );
     }
   }
 
