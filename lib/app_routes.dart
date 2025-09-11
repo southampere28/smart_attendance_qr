cd8334d001eed30c930e1450b81ccecc53f15ee9
Pramudya <pramudyap333@gmail.com>
2025-09-11 21:51:26 +0700

    feat: add new route (register student and register teacher)

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -4,8 +4,12 @@ import 'package:absensi_qr/features/dashboard/presentation/dashboard_binding.dar
 import 'package:absensi_qr/features/dashboard/presentation/dashboard_page.dart';
 import 'package:absensi_qr/features/navigation/presentation/navigation_binding.dart';
 import 'package:absensi_qr/features/navigation/presentation/navigation_page.dart';
-import 'package:absensi_qr/features/others/auth/presentation/login_binding.dart';
-import 'package:absensi_qr/features/others/auth/presentation/login_page.dart';
+import 'package:absensi_qr/features/others/auth/login/presentation/login_binding.dart';
+import 'package:absensi_qr/features/others/auth/login/presentation/login_page.dart';
+import 'package:absensi_qr/features/others/auth/register_student/presentation/register_student_binding.dart';
+import 'package:absensi_qr/features/others/auth/register_student/presentation/register_student_page.dart';
+import 'package:absensi_qr/features/others/auth/register_teacher/presentation/register_teacher_binding.dart';
+import 'package:absensi_qr/features/others/auth/register_teacher/presentation/register_teacher_page.dart';
 import 'package:absensi_qr/features/others/splash_screen/presentation/splash_screen_binding.dart';
 import 'package:absensi_qr/features/others/splash_screen/presentation/splash_screen_page.dart';
 import 'package:absensi_qr/features/permission/presentation/permission_binding.dart';
@@ -18,10 +22,12 @@ import 'package:get/get.dart';
 
 class AppRoutes {
   // initialize
-  static const initialRoute = login;
+  static const initialRoute = registerStudent;
   
   // auth
   static const login = '/login';
+  static const registerStudent = '/register-student';
+  static const registerTeacher = '/register-teacher';
   
   // general
   static const navigation = '/navigation';
@@ -73,5 +79,13 @@ class AppRoutes {
         name: login,
         page: () => const LoginPage(),
         binding: LoginBinding(),),
+    GetPage(
+        name: registerStudent,
+        page: () => const RegisterStudentPage(),
+        binding: RegisterStudentBinding(),),
+    GetPage(
+        name: registerTeacher,
+        page: () => const RegisterTeacherPage(),
+        binding: RegisterTeacherBinding(),),
   ];
 }
