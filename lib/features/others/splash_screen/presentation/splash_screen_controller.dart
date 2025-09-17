0aa86d9c7c888a5c03eb99940608b3775d893ffc
Pramudya <pramudyap333@gmail.com>
2025-09-17 09:28:06 +0700

    feat: improve splash page UI and show loading message

--- lib/features/others/splash_screen/presentation/splash_screen_controller.dart
+++ lib/features/others/splash_screen/presentation/splash_screen_controller.dart
@@ -1,9 +1,28 @@
+import 'package:absensi_qr/app_routes.dart';
+import 'package:absensi_qr/services/endpoint_service.dart';
 import 'package:get/get.dart';
 
 class SplashScreenController extends GetxController {
+  EndpointService endpointService = Get.find<EndpointService>();
+
+  RxString messageLoading = ''.obs;
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
+    _startSplash();
+  }
+
+  void _startSplash() async {
+    messageLoading.value = '🔄 Loading data awal...';
+    await endpointService.loadClasses();
+
+    messageLoading.value = '⚙️ Menyiapkan aplikasi untukmu...';
+    await Future.delayed(Duration(seconds: 3));
+
+    messageLoading.value = '✅ Selesai, menuju halaman login...';
+    await Future.delayed(Duration(seconds: 1));
+    Get.offNamed(AppRoutes.login);
   }
 }
--- lib/features/others/splash_screen/presentation/splash_screen_page.dart
+++ lib/features/others/splash_screen/presentation/splash_screen_page.dart
@@ -1,3 +1,4 @@
+import 'package:absensi_qr/constant/app_font_style.dart';
 import 'package:absensi_qr/features/others/splash_screen/presentation/splash_screen_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,10 +12,23 @@ class SplashScreenPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          'Splash Screen Page'
-        ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(children: [
+          Expanded(flex: 4, child: SizedBox()),
+          Text(
+            'Splash Screen Page',
+            style: AppFontStyle.titleText,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Obx(() => Text(
+                controller.messageLoading.value,
+                style: AppFontStyle.smallText,
+              )),
+          Expanded(flex: 3, child: SizedBox()),
+        ]),
       ),
     );
   }
