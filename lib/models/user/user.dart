ee83575dc935375a233152f0f7c00073624048dc
Pramudya <pramudyap333@gmail.com>
2025-09-25 16:22:03 +0700

    refactor: remove password field from user model

--- lib/models/user/user.dart
+++ lib/models/user/user.dart
@@ -1,59 +1,62 @@
 import 'dart:convert';
-
 import 'package:absensi_qr/models/user/student.dart';
 import 'package:absensi_qr/models/user/teacher.dart';
 
 class User {
-
-  final BigInt id;
+  final int id;
   final String role;
   final String? email;
-  final String password;
   final String? profilePicture;
   final DateTime? createdAt;
   final DateTime? updatedAt;
   final DateTime? deletedAt;
+  final DateTime? emailVerifiedAt;
   final Student? student;
   final Teacher? teacher;
-  
+
   User({
     required this.id,
     required this.role,
     this.email,
-    required this.password,
     this.profilePicture,
     this.createdAt,
     this.updatedAt,
     this.deletedAt,
+    this.emailVerifiedAt,
     this.student,
     this.teacher,
   });
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id.toString(),
+    return {
+      'id': id,
       'role': role,
       'email': email,
-      'password': password,
-      'profilePicture': profilePicture,
+      'profile_picture': profilePicture,
       'created_at': createdAt?.toIso8601String(),
       'updated_at': updatedAt?.toIso8601String(),
       'deleted_at': deletedAt?.toIso8601String(),
-      "student": student?.toString(),
-      "teacher": teacher?.toString(),
+      'email_verified_at': emailVerifiedAt?.toIso8601String(),
+      'student': student?.toMap(),
+      'teacher': teacher?.toMap(),
     };
   }
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
-      id: BigInt.parse(map['id'].toString()),
+      id: map['id'] is int ? map['id'] : int.parse(map['id'].toString()),
       role: map['role'] as String,
-      email: map['email'] != null ? map['email'] as String : null,
-      password: map['password'] as String,
-      profilePicture: map['profilePicture'] != null ? map['profilePicture'] as String : null,
-      createdAt: map['created_at'] != null ? DateTime.parse(map['created_at'].toString()) : null,
-      updatedAt: map['updated_at'] != null ? DateTime.parse(map['updated_at'].toString()) : null,
-      deletedAt: map['deleted_at'] != null ? DateTime.parse(map['deleted_at'].toString()) : null,
+      email: map['email'],
+      profilePicture: map['profile_picture'],
+      createdAt:
+          map['created_at'] != null ? DateTime.parse(map['created_at']) : null,
+      updatedAt:
+          map['updated_at'] != null ? DateTime.parse(map['updated_at']) : null,
+      deletedAt:
+          map['deleted_at'] != null ? DateTime.parse(map['deleted_at']) : null,
+      emailVerifiedAt: map['email_verified_at'] != null
+          ? DateTime.parse(map['email_verified_at'])
+          : null,
       student: map['student'] != null ? Student.fromMap(map['student']) : null,
       teacher: map['teacher'] != null ? Teacher.fromMap(map['teacher']) : null,
     );
@@ -61,5 +64,10 @@ class User {
 
   String toJson() => json.encode(toMap());
 
-  factory User.fromJson(String source) => User.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'User(id: $id, role: $role, email: $email, profilePicture: $profilePicture, createdAt: $createdAt, updatedAt: $updatedAt, deletedAt: $deletedAt, emailVerifiedAt: $emailVerifiedAt, student: $student, teacher: $teacher)';
+  }
 }
