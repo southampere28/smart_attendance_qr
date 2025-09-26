a9f642dfa971df0b9ba38e5b62d4bfff28dcadd0
Pramudya <pramudyap333@gmail.com>
2025-09-26 08:32:21 +0700

    test: add trial implementation for geolocation service on dashboard

--- lib/features/dashboard/presentation/dashboard_controller.dart
+++ lib/features/dashboard/presentation/dashboard_controller.dart
@@ -1,15 +1,24 @@
 import 'package:absensi_qr/services/endpoint_service.dart';
+import 'package:absensi_qr/services/geolocation_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
-class DashboardController extends GetxController{
+class DashboardController extends GetxController {
   final EndpointService _httpService = Get.find<EndpointService>();
+
+  final GeolocationService _geolocationService = Get.find<GeolocationService>();
 
   var isConnected = false.obs;
 
   Future<void> checkConnection() async {
     isConnected.value = await _httpService.testConnection();
   }
-  
+
+  // geolocation
+  Future<void> getLocation() async {
+    await _geolocationService.getCurrentPosition();
+  }
+
   @override
   void onInit() {
     // TODO: implement onInit
--- lib/features/dashboard/presentation/dashboard_page.dart
+++ lib/features/dashboard/presentation/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:absensi_qr/features/dashboard/presentation/dashboard_controller.dart';
 import 'package:absensi_qr/services/endpoint_service.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 class DashboardPage extends StatelessWidget {
@@ -10,16 +11,29 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     DashboardController controller = Get.find<DashboardController>();
 
-    return Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Dashboard Page'),
-          ElevatedButton(
-              onPressed: () {
-                controller.checkConnection();
-              },
-              child: Text('testconnection'))
-        ]);
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      width: double.infinity,
+      child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Dashboard Page'),
+            ElevatedButton(
+                onPressed: () {
+                  controller.checkConnection();
+                },
+                child: Text('testconnection')),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  // do something here
+                  await controller.getLocation();
+                },
+                child: Text('Check Status Location'))
+          ]),
+    );
   }
 }
