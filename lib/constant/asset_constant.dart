2ee7c5073b864bec6ed269d287b1c6ba76877463
Pramudya <pramudyap333@gmail.com>
2026-01-25 13:40:05 +0700

    feat: add assets and resolve flutter_secure_storage dependency version

---
+++ lib/constant/asset_constant.dart
@@ -0,0 +1,13 @@
+class AssetConstant {
+
+  // base path icon
+  static const String baseImagePath = 'assets/images';
+  static const String baseIconPath = 'assets/icons';
+
+  // icon core general
+  static const String iconApp = '$baseIconPath/icon_app.png';
+
+
+  /// icon per feature here...
+
+}
