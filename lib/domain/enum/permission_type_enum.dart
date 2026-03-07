41fbfc59c593d59f6cd145e570122697661d907c
Pramudya <pramudyap333@gmail.com>
2026-03-07 21:58:18 +0700

    feat: update permission type enum

--- lib/domain/enum/permission_type_enum.dart
+++ lib/domain/enum/permission_type_enum.dart
@@ -1,22 +1,29 @@
 enum PermissionTypeEnum {
   sakit,
   izin,
-  dispen;
+  dispen,
+  lainnya;
 
   static PermissionTypeEnum fromString(String value) {
     switch (value.toLowerCase()) {
-      case 'sakit':
       case 'sick':
+      case 'sakit':
         return PermissionTypeEnum.sakit;
 
+      case 'excused':
       case 'izin':
         return PermissionTypeEnum.izin;
 
       case 'dispen':
         return PermissionTypeEnum.dispen;
+      
+      case 'etc':
+      case 'lainnya':
+        return PermissionTypeEnum.lainnya;
 
       default:
         return PermissionTypeEnum.izin; // fallback aman
     }
   }
+
 }
