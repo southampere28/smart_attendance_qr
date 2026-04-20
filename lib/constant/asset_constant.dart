1ee3e95287eec8e000a7d2611e7b979bf034168d
Pramudya <pramudyap333@gmail.com>
2026-04-20 10:17:32 +0700

    feat: add asset icon

--- lib/constant/asset_constant.dart
+++ lib/constant/asset_constant.dart
@@ -1,5 +1,4 @@
 class AssetConstant {
-
   // base path icon
   static const String baseImagePath = 'assets/images';
   static const String baseIconPath = 'assets/icons';
@@ -10,5 +9,10 @@ class AssetConstant {
   /// icon per feature here...
   static const String svgIconQR = '$baseIconPath/qr_code_scanner.svg';
   static const String svgIconSubject = '$baseIconPath/icon_subject.svg';
+  static const String svgIconSubjectThin =
+      '$baseIconPath/icon_subject_thin.svg';
 
+  // attendance status icons
+  static const String iconAttValid = '$baseIconPath/ic-attendance-valid.svg';
+  static const String iconAttNone = '$baseIconPath/ic-attendance-none.svg';
 }
